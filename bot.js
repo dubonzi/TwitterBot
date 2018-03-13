c911a3b275f2374c9c7216a6a2c264f96148c077
Eduardo Bonzi <dubonzi@gmail.com>
2018-03-13 20:59:44 -0300

    Nova função para responder menções.

--- bot.js
+++ bot.js
@@ -7,34 +7,45 @@ var T = new Twit(config);
 
 /* Exemplo de stream com post */
 /* Descomentar as linhas abaixo para habilitar o stream */
-//var stream = T.stream('user');
-//stream.on('follow', followed);
+/* var stream = T.stream('user');
+stream.on('tweet', eventoTweet); */
 
-function followed(evento) {
-    let nome = evento.source.name;
-    let usuario = evento.source.screen_name;
-    console.log(usuario + " está seguindo.");
-    novoTweet('@' + usuario + ' obrigado pelo follow!');
+function eventoTweet(evento) {
+  var mensagem = evento.text;
+  var remetente = evento.user.screen_name;
+  var usuariosCitacao = evento.entities.user_mentions;
+  usuariosCitacao.forEach(usuario => {
+    if (usuario.screen_name === 'dubonzi_dev') {
+      console.log('@' + remetente + " falou comigo!");
+      novoTweet("Olá @" + remetente + " , obrigado pela sua mensagem!");
+    }
+  });
 }
 
 function novoTweet(mensagem) {
-    let status = {status: mensagem};
-    T.post('statuses/update', status, (erro, data, response) => {
-        if (erro) {
-            console.error(erro);
-        }
-    });
+  let status = {
+    status: mensagem
+  };
+  T.post('statuses/update', status, (erro, data, response) => {
+    if (erro) {
+      console.error(erro);
+    } else {
+      console.log("Mensagem enviada.");
+    }
+  });
 }
 
 /* Exemplo de get, salvando em arquivo, retorna o último tweet do bot */
-var params = {
-    screen_name: 'dubonzi_dev',
-    count: 1
+/* var params = {
+  screen_name: 'dubonzi_dev',
+  count: 1
 };
 
- T.get('statuses/user_timeline', params, function (err, data, response) {
-    fs.writeFile("resultado.json", JSON.stringify(data), (erroGravacao) => {
-        if (erroGravacao) console.error(erroGravacao);
-    });
-    console.log("Arquivo criado.");
-}); 
+T.get('statuses/user_timeline', params, function (err, data, response) {
+  fs.writeFile("ArquivosGerados/resultado.json", JSON.stringify(data), erroGravacaoArquivo);
+  console.log("Arquivo criado.");
+}); */
+
+function erroGravacaoArquivo(erro) {
+  if (erro) console.error(erro);
+}
--- config-empty.js
+++ config-empty.js
@@ -5,8 +5,8 @@
 // var config = require('./config.js');
 
 module.exports = {
-    consumer_key:         '', 
-    consumer_secret:      '',
-    access_token:         '',
-    access_token_secret:  ''
-  }
+  consumer_key: '',
+  consumer_secret: '',
+  access_token: '',
+  access_token_secret: ''
+}
