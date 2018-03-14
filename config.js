31378eeac0a01556157fbc1466e474774902c9e3
Eduardo Bonzi <dubonzi@gmail.com>
2018-03-13 21:33:51 -0300

    incluindo config

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  consumer_key: 'VM6Mws2UCKSSfubhBZ7thxYqB',
+  consumer_secret: '1qUGOJUy5UDnF6Pkofxx1i0BdNaJmOseBHq2P89keLomUxBb4O',
+  access_token: '973346371041361920-7rDAVxXaAZRzf4ISXngjywKMB0ksSby',
+  access_token_secret: 'kjFTZ0kNR9M2INDeuVRqNNCxIVSRsPlmYNDUxQaFHMj2X',
+  timeout_ms: 30 * 1000
+}
